787525325b2073e50a27308d3ad6df47b51b6274
Rolf Rander Næss <rolfn-github@pvv.org>
2021-04-11 20:14:40 +0200

    correlating polling and interrupts
    tried getting interrupts to work without luck. Implemented a
    simple oscilloscope using polling to discover that the interrupts
    are unreliablie.

--- test_rotary/app/application.cpp
+++ test_rotary/app/application.cpp
@@ -8,6 +8,7 @@
 #define WIFI_PWD "Kieslowski"
 #endif
 
+#define VERSION "v0.3"
 #define DISABLE_WIFI
 
 #define LED_PIN 2 // GPIO2
@@ -17,7 +18,7 @@
 #define ROT_B_PIN 2   // GPIO2 = D4
 
 /*
-Pinout:
+Pinout display:
 MISO GPIO12 (oransj -> 7)
 MOSI GPIO13 (blå    -> 6)
 CLK GPIO14  (grønn  -> 8)
@@ -32,57 +33,95 @@ LED, bakgrunnslys (gul)
 
 Adafruit_ILI9341 tft;
 
-bool state = true;
-
-bool state_a = false;
-bool state_b = false;
-bool state_sw = false;
-
-String txt_a_on  = "ROT-A: On ";
-String txt_a_off = "ROT-A: Off";
-String txt_b_on  = "ROT-B: On ";
-String txt_b_off = "ROT-B: Off";
-String txt_sw_on  = "Switch pressed";
-String txt_sw_off = "Switch neutral";
-
-void blink()
-{
-	digitalWrite(LED_PIN, state);
-	state = !state;
-}
-
-
 Timer guiTimer;
-Timer procTimer;
 
-int counter=0;
+String events[] = {"Counter", NULL};
+String inputs[] = {"Input", "button", "rot-A", "rot-B", "rotation", NULL};
 
-void clearAndWrite(int x, int y, int textSize, int textColor, String text) {
-	tft.setTextSize(textSize);
-	tft.setTextColor(textColor);
-	tft.fillRect(x, y, text.length()*textSize*6, textSize*8, ILI9341_DARKGREY);
-	tft.setCursor(x, y);
-	tft.print(text);
+#define CNT_BUT 0
+#define CNT_ROT_A 1
+#define CNT_ROT_B 2
+#define CNT_VAL 3
+
+const int numCounters = 4;
+int counters[] = { 
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+
+const inline int maxInt(int a, int b) {
+	return (a>b) ? a : b;
 }
 
-void displayStatus()
-{
-	String status = String(counter);
-	int line = 0;
-	clearAndWrite(15, 15 + line*30, 3, ILI9341_GREEN, status);
-	line++;
-	clearAndWrite(15, 15 + line*30, 3, ILI9341_WHITE, state_sw ? txt_sw_on : txt_sw_off);
-	line++;
-	clearAndWrite(15, 15 + line*30, 3, ILI9341_WHITE, state_a ? txt_a_on : txt_a_off);
-	line++;
-	clearAndWrite(15, 15 + line*30, 3, ILI9341_WHITE, state_b ? txt_b_on : txt_b_off);
+const inline int minInt(int a, int b) {
+	return (a<b) ? a : b;
 }
 
-void initDisplay() {
-	// text display tests
-	tft.begin();
-	tft.fillScreen(0);
-	tft.setRotation(1);
+class Grid {
+	private:
+		int margin;
+		int firstColWith;
+		int colWith;
+		int textSize;
+		Adafruit_GFX *gfx;
+
+		void drawCell(int col, int row, String txt, int foreground, int background) {
+			this->gfx->setTextSize(this->textSize);
+			this->gfx->setTextColor(foreground);
+			int x = this->margin;
+			if(col > 0) { x += firstColWith; }
+			if(col > 1) { x += colWith * (col-1); }
+			int y = this->margin + row * textSize * 8;
+			int width = (col == 0 ? firstColWith : colWith) * textSize * 6;
+			int height = textSize * 8;
+			this->gfx->fillRect(x, y, width, height, background);
+			this->gfx->setCursor(x, y);
+			this->gfx->print(txt);
+		}
+
+	public:
+		Grid(Adafruit_GFX *_gfx, int margin) {
+			this->gfx = _gfx;
+			this->margin = margin;
+		}
+
+		void init(String leftCol[], String topRow[]) {
+			this->gfx->fillScreen(ILI9341_BLACK);
+
+			int maxLeftCol = 0;
+			int rows = 0;
+			for(int i=0; leftCol[i]; i++) {
+				maxLeftCol = maxInt(maxLeftCol, leftCol[i].length()+1);
+				rows++;
+			}
+			int maxTopRow = 0;
+			int cols = 0; // this does not include leftmost column
+			for(int i=0; topRow[i]; i++) {
+				maxTopRow = maxInt(maxTopRow, topRow[i].length()+1);
+				cols++;
+			}
+			int maxCharWith = (gfx->width() - this->margin*2) / (maxLeftCol + maxTopRow*cols);
+			int maxCharHeight = (gfx->height() - this->margin*2) / rows;
+			this->textSize = minInt(maxCharWith / 6, maxCharHeight / 8);
+
+			this->firstColWith = maxLeftCol * this->textSize * 6;
+			this->colWith = maxTopRow * this->textSize * 6;
+
+			for(int i=0; i<rows; i++) {
+				this->drawCell(0, i, leftCol[i], ILI9341_WHITE, ILI9341_DARKGREY);
+			}
+			for(int i=0; i<cols; i++) {
+				this->drawCell(i+1, 0, topRow[i], ILI9341_YELLOW, ILI9341_BLUE);
+			}
+		}
+
+		void updatecell(int col, int row, String text) {
+			this->drawCell(col+1, row+1, text, ILI9341_GREEN, ILI9341_BLACK);
+		}
+};
+
+void splashScreen() {
+		// text display tests
 	tft.setTextSize(2);
 
 	tft.setTextColor(ILI9341_GREEN);
@@ -90,32 +129,177 @@ void initDisplay() {
 	tft.setCursor(60, 60);
 	tft.print("Rotary switch demo");
 	tft.setCursor(60, 80);
-	tft.print("v0.11");
+	tft.print(VERSION);
 	tft.setTextColor(ILI9341_WHITE);
 	tft.setCursor(60, 120);
 	tft.print("rrn");
-	delay(2000);
-	tft.fillScreen(0);
 }
 
+Grid *grid;
+
+void initDisplay() {
+	tft.begin();
+	tft.fillScreen(0);
+	tft.setRotation(1);
+
+	splashScreen();
+	delay(2000);
+}
+
+void initGrid() {
+	grid = new Grid(&tft, 5);
+	grid->init(inputs, events);
+}
+
+bool pendingUpdate[] = {false, false, false, false};
+
+void clearPending(void *pending) {
+	*(bool *)pending = false;
+}
+
+SimpleTimer clearTimers[] {
+	SimpleTimer(), 
+	SimpleTimer(), 
+	SimpleTimer(), 
+	SimpleTimer()
+};
+
+void initTimers() {
+	for(uint32_t i=0; i<numCounters; i++) {
+		clearTimers[i].initializeMs(500, clearPending, &pendingUpdate[i]);
+	}
+}
+
+void doUpdateCounter(uint32_t c) {
+	counters[c]++;
+	grid->updatecell(0, c, String(counters[c]));
+	if(c == CNT_BUT) {
+		for(int i=1; i<numCounters; i++) {
+			counters[i] = 0;
+			grid->updatecell(0, i, "0");
+		}
+	}
+
+	if(c == CNT_ROT_A) {
+		if(digitalRead(ROT_B_PIN)) { 
+			counters[CNT_VAL]++;
+		} else {
+			counters[CNT_VAL]--;
+		}
+		grid->updatecell(0, CNT_VAL, String(counters[CNT_VAL]));
+	}
+	// pendingUpdate[c] = false;
+	clearTimers[c].start(false);
+}
+
+void updateCounter(uint32_t c) {
+	if(!pendingUpdate[c]) {
+		pendingUpdate[c] = true;
+		System.queueCallback(doUpdateCounter, c);
+	}
+}
+
+void IRAM_ATTR intSwitch() { updateCounter(CNT_BUT);   };
+void IRAM_ATTR intRotA()   { updateCounter(CNT_ROT_A); };
+void IRAM_ATTR intRotB()   { updateCounter(CNT_ROT_B); };
+
 void initPins() {
+
+	// note to self: only one interrupt per pin
+	attachInterrupt(SWITCH_PIN, intSwitch, FALLING);
+	attachInterrupt(ROT_A_PIN, intRotA, FALLING);
+	attachInterrupt(ROT_B_PIN, intRotB, FALLING);
+
+	// attachInterrupt sets pinMode, so this must be reset after interrupt setup
+	pinMode(SWITCH_PIN, INPUT_PULLUP);
+	pinMode(ROT_A_PIN, INPUT_PULLUP);
+	pinMode(ROT_B_PIN, INPUT_PULLUP);
+
+}
+
+void updateStatus() {
+	for(int i=0; i<numCounters; i++) {
+		grid->updatecell(0, i, String(counters[i]));
+	}
+}
+
+int pin_a[320];
+int pin_b[320];
+int count=0;
+Timer pollTimer;
+int offset = 0;
+int mark_a = -1;
+int mark_b = -1;
+
+void markA() {
+	mark_a = count;
+}
+
+void markB() {
+	mark_b = count;
+}
+
+void initPinsPoll() {
+	attachInterrupt(ROT_A_PIN, markA, FALLING);
+	attachInterrupt(ROT_B_PIN, markB, FALLING);
+
 	pinMode(SWITCH_PIN, INPUT_PULLUP);
 	pinMode(ROT_A_PIN, INPUT_PULLUP);
 	pinMode(ROT_B_PIN, INPUT_PULLUP);
 }
 
-void updateStatus() {
-	counter++;
-	state_sw= digitalRead(SWITCH_PIN);
-	state_a = digitalRead(ROT_A_PIN);
-	state_b = digitalRead(ROT_B_PIN);
-	displayStatus();
+void graph(Adafruit_GFX* gfx, int offset, int data[], int count) {
+	int m = minInt(count, gfx->width());
+	int16_t y = offset + 7;
+	for(int16_t x=0; x<m; x++) {
+		gfx->drawPixel(x, y-data[x]*5, ILI9341_WHITE);
+	}
+}
+
+void initGraphs() {
+	tft.fillScreen(ILI9341_BLACK);
+}
+
+void drawGraphs(void *) {
+	if(offset == 0) {
+		tft.fillScreen(ILI9341_BLACK);
+	}
+	if(mark_a > 0) {
+		tft.drawLine(mark_a, offset+1, mark_a, offset+8, ILI9341_PINK);
+	}
+	graph(&tft, offset, pin_a, 320);
+	if(mark_b > 0) {
+		tft.drawLine(mark_b, offset+11, mark_b, offset+18, ILI9341_YELLOW);
+	}
+	graph(&tft, offset+10, pin_b, 320);
+	mark_a = -1;
+	mark_b = -1;
+	offset += 25;
+	if(offset+10 > tft.height()) {
+		offset = 0;
+	}
+}
+
+void poll() {
+	bool a = !digitalRead(ROT_A_PIN);
+	bool b = !digitalRead(ROT_B_PIN);
+	if(a || b || (count>0)) {
+		count++;
+		pin_a[count] = a ? 1 : 0;
+		pin_b[count] = b ? 1 : 0;
+	}
+	if(count >= 319) {
+		count = 0;
+		pin_a[0] = 0;
+		pin_b[0] = 0;
+		System.queueCallback(drawGraphs);
+	}
 }
 
 void init()
 {
-//	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
-//	Serial.systemDebugOutput(true); // Allow debug output to serial
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(true); // Allow debug output to serial
 
 #ifndef DISABLE_WIFI
 	WifiStation.config(WIFI_SSID, WIFI_PWD);
@@ -123,9 +307,18 @@ void init()
 	WifiAccessPoint.enable(false);
 #endif
 
+	// Serial.println("init display");
 	initDisplay();
-	initPins();
-	guiTimer.initializeMs(1000, updateStatus).start();
+	initGraphs();
+	// initGrid();
+	// Serial.println("init debounce timers");
+	// initTimers();
+	Serial.println("init pins and interrupts");
+	// initPins();
+	initPinsPoll();
+	pollTimer.initializeUs(500, poll).start();
+	// Serial.println("start timer");
+	//guiTimer.initializeMs(1000, updateStatus).start();
 }
 
 
