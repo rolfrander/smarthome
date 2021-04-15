8a9a0ce7aad667b0a8ef64f80c7661d0dd3346ea
Rolf Rander Næss <rolfn-github@pvv.org>
2021-04-15 22:16:11 +0200

    basic framework

---
+++ spi_display/app/application.cpp
@@ -0,0 +1,21 @@
+#include <SmingCore.h>
+
+
+/*
+Pinout display:
+MISO SD0 (oransj -> 8)
+MOSI SD1 (blå    -> 10)
+CLK  CLK (grønn  -> 7)
+CS   CMD (hvit   -> 9)
+DC   ?   (lilla  -> )
+RST  ?   (grå    -> )
+
+VCC (brun)
+GND (svart)
+LED, bakgrunnslys (gul)
+*/
+
+
+void init()
+{
+}
