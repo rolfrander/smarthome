32bb3fe713652fcb4e382472649416005698e8c8
Rolf Rander Naess <rolfn-github@pvv.org>
2021-04-20 13:30:35 +0000

    first attempt at basic gpio testing

--- test_gpio/app/application.cpp
+++ test_gpio/app/application.cpp
@@ -1,18 +1,53 @@
 #include <SmingCore.h>
 
-#define LED_PIN 2 // GPIO2
-
 Timer procTimer;
 bool state = true;
+uint8_t gpio = 2;
+
+uint8_t next[] {
+	4, // 0
+	0, // 1
+	0, // 2
+	0, // 3
+	5, // 4
+	16,// 5
+	0, // 6
+	0, // 7
+	0, // 8
+	0, // 9
+	0, // 10
+	0, // 11
+	0, // 12
+	0, // 13
+	0, // 14
+	0, // 15
+	2, // 16
+};
 
 void blink()
 {
-	digitalWrite(LED_PIN, state);
+	digitalWrite(gpio, state);
+
+	if(state) {
+		Serial.printf("gpio %2d on\n", gpio);
+	} else {
+		Serial.printf("gpio %2d off\n", gpio);
+		gpio = next[gpio];
+	}
 	state = !state;
 }
 
 void init()
 {
-	pinMode(LED_PIN, OUTPUT);
+    WifiAccessPoint.enable(false);
+
+	Serial.begin(115200);
+	// Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(true); // Allow debug output to serial
+
+	for(int i=0; i<=16; i++) {
+		pinMode(i, OUTPUT);
+	}
+	gpio = 2;
 	procTimer.initializeMs(1000, blink).start();
 }
